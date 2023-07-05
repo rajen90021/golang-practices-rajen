b35a4d477977efde0e7b93ff09e80fc87fcd589a
rajen <grajen90021@gmail.com>
2023-07-05 11:43:23 +0530

    added

---
+++ golang-basic/webrequest.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://lco.dev"
+
+func main() {
+	fmt.Println("loc web request")
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("response is of type %T\n", response)
+	defer response.Body.Close()
+	databytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	content := string(databytes)
+	fmt.Println(content)
+}
